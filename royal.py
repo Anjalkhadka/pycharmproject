ffb3fb072d15ccfac19d78dc3c0052c7d4ccfc6e
anjalkhadka <Anjalkhadka>
2021-06-10 14:12:58 +0545

    integer

---
+++ royal.py
@@ -0,0 +1,8 @@
+
+N = int(input('enter the minutes passed since midnight:'))
+hours = (N//60)
+minutes = (N%60)
+
+print(f'the hour is {hours}')
+print(f'the number is {minutes}')
+print(f'Its {hours}:{minutes} now')

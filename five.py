7b26fca1731343365192d2eef4c928cf5d237df5
anjalkhadka <Anjalkhadka>
2021-06-14 10:24:47 +0545

    integer

---
+++ five.py
@@ -0,0 +1,6 @@
+'''
+write a python program to find sum of the first n positive integers.
+sum=(n*(n+1)//2'''
+n=int(input('enter the first n positive integers:'))
+sum=(n*(n+1)//2)
+print(f'the sum of first n positve number is {sum}')
---
+++ four.py
@@ -0,0 +1,11 @@
+'''
+ If temperature is greater than 30, it's a hot day other wise if it's less than 10;
+ it's a cold day; otherwise, it's neither hot nor cold.
+'''
+tem=int(input('enter the num of tem:'))
+if tem>30:
+    print('it is the hot day')
+else:
+    print('it is the cold day')
+
+
